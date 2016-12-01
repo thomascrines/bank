b161b65ad08b27773646463637b10514662ddbbf
Thomas Crines <thomascrines@hotmail.com>
2016-12-01 15:21:27 +0000

    add find acc by name

--- bank.js
+++ bank.js
@@ -1,5 +1,18 @@
 var Bank = function() {
-  this.accounts = [];
+  this.accounts = []; 
+}
+
+Bank.prototype = {
+  addAccount: function(account) {
+    this.accounts.push(account);
+  },
+
+  findAccountByName: function(name){
+      var account = this.accounts.find( function(account) {
+        return account.name === name
+      })
+      return account;
+    }  
 }
 
 module.exports = Bank
--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -1,21 +1,24 @@
 var Bank = require('../Bank');
 var assert = require('assert');
 
-var bank;
-
 describe('Bank function', function() {
+
+  var bank;
+  var account1;
+  var account2;
+  var account3;
 
   beforeEach(function() {
     bank = new Bank();
-    var account1 = {
+    account1 = {
       name: 'Bob',
       amount: 20.00,
     };
-    var account2 = {
+    account2 = {
       name: 'Randall',
       amount: 100.00,
     };
-    var account3 = {
+    account3 = {
       name: 'Betty',
       amount: 200.00,
     };
@@ -23,6 +26,18 @@ describe('Bank function', function() {
 
   it('accounts start empty', function() {
     assert.equal(0, bank.accounts.length);
+  })
+
+  it('account can be added to bank', function() {
+    bank.addAccount(account1);
+    assert.equal(1, bank.accounts.length);
+  })
+
+  it('find account by name', function(){
+    bank.addAccount(account1);
+    bank.addAccount(account2);
+    bank.addAccount(account3);
+    assert.equal( account3, bank.findAccountByName("Betty") );
   })
 });
 
